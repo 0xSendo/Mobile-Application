ace24edc181d00e9f7ceadf002179efaaa2879a9
0xSendo <142055962+0xSendo@users.noreply.github.com>
2025-02-25 01:27:31 +0800

    Update TaskViewModel.kt

--- Kotlin/TaskViewModel.kt
+++ Kotlin/TaskViewModel.kt
@@ -1,5 +1,3 @@
-// TaskViewModel.kt
-
 package com.example.myacademate
 
 import androidx.compose.runtime.getValue
@@ -8,28 +6,36 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class TaskViewModel : ViewModel() {
-    val taskList = TaskRepository.taskList
+    // Wrap TaskRepository.taskList in a reactive MutableState
+    private val _taskList = mutableStateOf(TaskRepository.taskList)
+    val taskList: List<DatabaseHelper.Task> by _taskList // Expose as reactive state
+
     var editingTaskId by mutableStateOf<Int?>(null)
         private set
 
     fun addTask(task: DatabaseHelper.Task) {
         TaskRepository.addTask(task)
+        _taskList.value = TaskRepository.taskList
     }
 
     fun updateTask(updatedTask: DatabaseHelper.Task) {
         TaskRepository.updateTask(updatedTask)
+        _taskList.value = TaskRepository.taskList // Update reactive state
     }
 
     fun deleteTask(taskId: Int) {
         TaskRepository.deleteTask(taskId)
+        _taskList.value = TaskRepository.taskList // Update reactive state
     }
 
     fun toggleTaskStatus(taskId: Int) {
         TaskRepository.toggleTaskStatus(taskId)
+        _taskList.value = TaskRepository.taskList // Update reactive state
     }
 
     fun updateTaskCompletion(taskId: Int, newCompletionPercentage: Int) {
         TaskRepository.updateTaskCompletion(taskId, newCompletionPercentage)
+        _taskList.value = TaskRepository.taskList // Update reactive state
     }
 
     fun startEditingTask(taskId: Int) {
